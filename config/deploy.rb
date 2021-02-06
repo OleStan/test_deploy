2c5b386f06eaa4270f04cc8e2a190096c29ab1f3
OleStan <59766354+OleStan@users.noreply.github.com>
2021-02-06 13:58:54 +0200

    next

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,39 +1,24 @@
-# config valid for current version and patch releases of Capistrano
 lock "~> 3.15.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :repo_url, "git@github.com:OleStan/test_deploy.git"
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :user, "deployer"
+set :rvm_ruby_version, "2.7.2"
+set :pty, true
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :linked_files, fetch(:linked_files, []).push("config/database.yml", "config/secrets.yml", "config/master.key", "config/puma.rb")
+set :linked_dirs,  fetch(:linked_dirs, []).push("log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads", "public/images", "storage")
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
+set :config_example_suffix, ".example"
+set :config_files, ["config/database.yml", "config/secrets.yml"]
+set :nginx_use_ssl, false
 
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+namespace :deploy do
+  before 'check:linked_files', 'set:master_key'
+  before 'check:linked_files', 'config:push'
+  before 'check:linked_files', 'puma:jungle:setup'
+  before 'check:linked_files', 'puma:nginx_config'
+end
+after "deploy:finished", "nginx:restart"
+after "deploy:finished", "puma:start"
